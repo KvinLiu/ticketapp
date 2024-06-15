9edbe0349735fe5e6f80a76d3912c0ef46746773
Kevin Liu <kevin07liu@gmail.com>
2024-06-15 22:41:51 +0800

    feat: modify ticket card

--- components/TicketPriority.tsx
+++ components/TicketPriority.tsx
@@ -11,7 +11,7 @@ const priorityMap: Record<Priority, { label: string; level: 1 | 2 | 3 }> = {
 };
 const TicketPriority = ({ priority }: Props) => {
   return (
-    <>
+    <div className="flex justify-between">
       <Flame
         className={`${
           priorityMap[priority].level >= 1 ? "text-red-500" : "text-muted"
@@ -27,7 +27,7 @@ const TicketPriority = ({ priority }: Props) => {
           priorityMap[priority].level >= 3 ? "text-red-500" : "text-muted"
         }`}
       ></Flame>
-    </>
+    </div>
   );
 };
 
