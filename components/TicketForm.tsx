059f728a50c1bd1488988f11da91fd8e804033e9
Kevin Liu <kevin07liu@gmail.com>
2024-06-15 21:57:38 +0800

    feat(api): add update ticket fn

--- components/TicketForm.tsx
+++ components/TicketForm.tsx
@@ -19,10 +19,15 @@ import { Button } from "./ui/button";
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { Ticket } from "@prisma/client";
 
 type TicketFormData = z.infer<typeof ticketSchema>;
 
-const TicketForm = () => {
+interface Props {
+  ticket?: Ticket;
+}
+
+const TicketForm = ({ ticket }: Props) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [error, setError] = useState("");
   const router = useRouter();
@@ -34,7 +39,11 @@ const TicketForm = () => {
     try {
       setIsSubmitting(true);
       setError("");
-      await axios.post("/api/tickets", values);
+      if (ticket) {
+        await axios.patch("/api/tickets/" + ticket.id, values);
+      } else {
+        await axios.post("/api/tickets", values);
+      }
       setIsSubmitting(false);
       router.push("/tickets");
       router.refresh();
@@ -52,6 +61,7 @@ const TicketForm = () => {
             <FormField
               control={form.control}
               name="title"
+              defaultValue={ticket?.title}
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Ticket Title</FormLabel>
@@ -65,6 +75,7 @@ const TicketForm = () => {
           <Controller
             name="description"
             control={form.control}
+            defaultValue={ticket?.description}
             render={({ field }) => (
               <SimpleMDE placeholder="Decription" {...field} />
             )}
@@ -73,6 +84,7 @@ const TicketForm = () => {
             <FormField
               control={form.control}
               name="status"
+              defaultValue={ticket?.status}
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Status</FormLabel>
@@ -97,6 +109,7 @@ const TicketForm = () => {
             <FormField
               control={form.control}
               name="priority"
+              defaultValue={ticket?.priority}
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Priority</FormLabel>
@@ -120,7 +133,7 @@ const TicketForm = () => {
             />
           </div>
           <Button type="submit" disabled={isSubmitting}>
-            Submit
+            {ticket ? "Update Ticket" : "Create Ticket"}
           </Button>
         </form>
       </Form>
