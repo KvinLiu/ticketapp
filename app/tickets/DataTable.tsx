4e96f9215488f48edd5bc85646650fb82b80a2bb
Kevin Liu <kevin07liu@gmail.com>
2024-06-15 17:48:28 +0800

    feat: add priority comp

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -1,3 +1,4 @@
+import TicketPriority from "@/components/TicketPriority";
 import TicketStatusBadge from "@/components/TicketStatusBadge";
 import {
   Table,
@@ -23,7 +24,10 @@ const DataTable = ({ tickets }: Props) => {
               <TableHead>
                 <div className="flex justify-center">Status</div>
               </TableHead>
-              <TableHead>Priority</TableHead>
+
+              <TableHead>
+                <div className="flex justify-center">Priority</div>
+              </TableHead>
               <TableHead>Created At</TableHead>
             </TableRow>
           </TableHeader>
@@ -37,7 +41,11 @@ const DataTable = ({ tickets }: Props) => {
                         <TicketStatusBadge status={ticket.status} />
                       </div>
                     </TableCell>
-                    <TableCell>{ticket.priority}</TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketPriority priority={ticket.priority} />
+                      </div>
+                    </TableCell>
                     <TableCell>
                       {ticket.createdAt.toLocaleDateString("en-US", {
                         year: "2-digit",
